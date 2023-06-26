5722b89fc1158be2107aba451bfb6164eb4995c0
BIYYALA HARI KRISHNA <harikrishnabiyyala@gmail.com>
2023-06-26 23:54:36 +0530

    listview and detailview done

---
+++ Readme.md
@@ -0,0 +1,11 @@
+### Django Rest API
+
+ Reference 
+ https://blog.logrocket.com/django-rest-framework-create-api/
+
+
+For virtual environment creation and activation
+
+ python3 -m venv myenv
+
+ source ./myenv/bin/activate
---
+++ todo_api/urls.py
@@ -0,0 +1,12 @@
+# todo/todo_api/urls.py : API urls.py
+
+from django.urls import path
+from .views import (
+    TodoListApiView,
+    TodoDetailApiView
+)
+
+urlpatterns = [
+    path('api', TodoListApiView.as_view()),
+    path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
+]
